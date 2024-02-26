a5baa8ade8648d6143ca605879cea11910447bea
0xChesterLee <chesterlee@cyberlab.host>
2024-02-26 23:57:08 +0800

    modified:   data.db 	modified:   misc.py 	modified:   rewriter.py

--- rewriter.py
+++ rewriter.py
@@ -22,9 +22,15 @@ def productRewriter(title, body, tags):
     response = response.choices[0].message.content.strip()
 
     productRewriter = {}
-    productRewriter['title'] = response.split("TITLE: ")[1].split("\n")[0]
-    productRewriter['body'] = response.split("BODY: ")[1].split("TAGS: ")[0].strip()
-    productRewriter['tags'] = response.split("TAGS: ")[1].strip()
+    productRewriter['title'] = response.split('TITLE: ')[1].split('\n')[0]
+    productRewriter['body'] = response.split('BODY: ')[1]
+
+    tags_start_index = response.find('TAGS: ')
+    if tags_start_index != -1:
+        tags_end_index = response.find('\n', tags_start_index)
+        productRewriter['tags'] = response[tags_start_index + len('TAGS: '):tags_end_index].strip()
+    else:
+        productRewriter['tags'] = ''
 
     return productRewriter
 
