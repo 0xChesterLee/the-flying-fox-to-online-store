a86338ecf830b2a631ef66aea3ae222b38c4a5f6
0xChesterLee <chesterlee@cyberlab.host>
2024-03-10 11:48:32 +0800

    Files Optimization

--- misc.py
+++ misc.py
@@ -22,18 +22,18 @@ TFF_COLLECTIONS_ENDPOINT = 'collections.json'
 TFF_PRODUCTS_ENDPOINT = 'products.json'
 
 # Scraped Products Data JSON File Name
-SCRAPED_JSON_FILENAME = 'ScrapedProductsData.json'
+SCRAPED_JSON_FILENAME = 'data/ScrapedProductsData.json'
 
 # Rewrite Products Data JSON File Name
-REWRITE_JSON_FILENAME = 'RewriteProductsData.json'
+REWRITE_JSON_FILENAME = 'data/RewriteProductsData.json'
 
 # System SQLite Database File Name
-DB_FILENAME = 'data.db'
+DB_FILENAME = 'data/data.db'
 DB_SCRAPE_TABLE_NAME = 'scrape_data'
 DB_REWRITE_TABLE_NAME = 'rewrite_data'
 
 # OpenAI API Key File Name
-OPENAI_API_KEY_FILE_NAME = '.openai-secret'
+OPENAI_API_KEY_FILE_NAME = 'secret/.openai-secret'
 
 # OpenAI Rewrite Prompt
 REWRITE_PROMPT = '請使用繁體中文重寫以下文字，但保留"TITLE:"、"TAGS:"和"BODY:"。"TITLE:"和"BODY:"需要保留70%以上的繁體中文字、"TAGS:"需要保留100%的繁體中文字，並將重寫後的內容輸出。在重寫後的"TAGS:"內容上，根據"BODY:"後的內容額外生成最多6個主題標籤。如果重寫後的"TAGS:"內容為空白，請自行生成最多6個主題標籤，主題標籤不需要加上"#"字符號，但請用逗號分隔。\nTITLE:{0}\nTAGS:{2}\nBODY:{1}'
@@ -50,8 +50,8 @@ PRODUCTS_DISCOUNT_RATE = 0.85
 FACEBOOK_PAGE_ID = 275521235637171
 
 # Facebook Page Access Token File Name And Endpoint URL
-FACEBOOK_TOKEN_FILE_NAME = '.facebook-secret'
+FACEBOOK_TOKEN_FILE_NAME = 'secret/.facebook-secret'
 FACEBOOK_POST_ENDPOINT_URL = f"https://graph.facebook.com/{FACEBOOK_PAGE_ID}/feed"
 
 # Cloudinary Access Information File Name
-CLOUDINARY_ACCESS_INFO_FILE_NAME = '.cloudinary-secret'
+CLOUDINARY_ACCESS_INFO_FILE_NAME = 'secret/.cloudinary-secret'
